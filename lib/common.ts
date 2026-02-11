dfe4c94bee787e131401410ec31c2fd18b3013d8
mkhotamirais <mkhotamirais@gmail.com>
2026-02-11 13:17:34 +0700

    email connect

--- lib/common.ts
+++ lib/common.ts
@@ -1,4 +1,4 @@
 export const links = {
-  email: { label: "email@email.com", url: "https://goggle.com" },
+  email: { label: "contact@vryce.id", url: "https://mailto:contact@vryce.id" },
   wa: { label: "+62 896-1158-0649", url: "https://wa.me/6287766606133" },
 };
