ab07356d6232e4d52d37c869caffbc94a912200f
mkhotamirais <mkhotamirais@gmail.com>
2026-02-21 15:37:19 +0700

    blog5

--- app/[locale]/(public)/blog/detail/[slug]/page.tsx
+++ app/[locale]/(public)/blog/detail/[slug]/page.tsx
@@ -14,10 +14,10 @@ export const generateMetadata = async ({ params }: { params: Promise<{ slug: str
   return { title: smartTrim(blog?.title || "", 60), description: smartTrim(blog?.content || "", 160) };
 };
 
-export const generateStaticParams = async () => {
-  const { blogs } = await getBlogs();
-  return blogs.map((blog) => ({ slug: blog.slug }));
-};
+// export const generateStaticParams = async () => {
+//   const { blogs } = await getBlogs();
+//   return blogs.map((blog) => ({ slug: blog.slug }));
+// };
 
 export default async function BlogDetail({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug;
