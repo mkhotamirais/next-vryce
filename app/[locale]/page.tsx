701fb09220d0ef82f089c062ae53e0580b4e3e1a
mkhotamirais <mkhotamirais@gmail.com>
2026-02-25 00:28:57 +0700

    akhir

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,17 +1,17 @@
 import HomeHero from "@/components/home/HomeHero";
-import HomeContactUs from "@/components/home/HomeContactUs";
-import HomeOurClients from "@/components/home/HomeOurClients";
-import HomeOurService from "@/components/home/HomeServices";
-import HomeTalent from "@/components/home/HomeTalent";
-import HomeWhyVrice from "@/components/home/HomeWhyVrice";
+// import HomeContactUs from "@/components/home/HomeContactUs";
+// import HomeOurClients from "@/components/home/HomeOurClients";
+// import HomeService from "@/components/home/HomeServices";
+// import HomeTalent from "@/components/home/HomeTalent";
+// import HomeWhyVrice from "@/components/home/HomeWhyVrice";
 import { setRequestLocale } from "next-intl/server";
 
-// import dynamic from "next/dynamic";
-// const HomeOurClients = dynamic(() => import("@/components/home/HomeOurClients"));
-// const HomeWhyVrice = dynamic(() => import("@/components/home/HomeWhyVrice"));
-// const HomeServices = dynamic(() => import("@/components/home/HomeServices"));
-// const HomeTalent = dynamic(() => import("@/components/home/HomeTalent"));
-// const HomeContactUs = dynamic(() => import("@/components/home/HomeContactUs"));
+import dynamic from "next/dynamic";
+const HomeOurClients = dynamic(() => import("@/components/home/HomeOurClients"));
+const HomeWhyVrice = dynamic(() => import("@/components/home/HomeWhyVrice"));
+const HomeServices = dynamic(() => import("@/components/home/HomeServices"));
+const HomeTalent = dynamic(() => import("@/components/home/HomeTalent"));
+const HomeContactUs = dynamic(() => import("@/components/home/HomeContactUs"));
 import { use } from "react";
 
 type Props = {
@@ -26,7 +26,7 @@ export default function Home({ params }: Props) {
     <div className="scroll-mt-12 md:scroll-mt-16">
       <HomeHero />
       <HomeWhyVrice />
-      <HomeOurService />
+      <HomeServices />
       <HomeTalent />
       <HomeOurClients />
       <HomeContactUs />
