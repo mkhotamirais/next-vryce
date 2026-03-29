e61e42947a84390db1e722a7ad72def6dbdebb8f
mkhotamirais <mkhotamirais@gmail.com>
2026-03-29 22:16:56 +0700

    logo height

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,9 +7,9 @@ export default function Logo({ variant = "default" }: { variant?: "default" | "d
     <div>
       <Link href="/" className="text-2xl font-bold">
         {variant === "dark" ? (
-          <Image src="/images/logos/LOGO.png" alt="Vryce Logo" width={100} height={120} className="w-30" />
+          <Image src="/images/logos/LOGO.png" alt="Vryce Logo" width={100} height={27} className="w-30" />
         ) : (
-          <Image src="/images/logos/BONUS_LOGO.png" alt="Vryce Logo" width={100} height={100} className="w-24" />
+          <Image src="/images/logos/BONUS_LOGO.png" alt="Vryce Logo" width={100} height={26} className="w-24" />
         )}
       </Link>
     </div>
