7c5c2c2507028c2e8faa5d0f3f2620b018f323a2
mkhotamirais <mkhotamirais@gmail.com>
2026-08-19 15:38:22 +0700

    logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,9 +7,21 @@ export default function Logo({ variant = "default" }: { variant?: "default" | "d
     <div>
       <Link href="/" className="text-2xl font-bold">
         {variant === "dark" ? (
-          <Image src="/images/logos/LOGO.png" alt="Vryce Logo" width={100} height={27} className="w-30" />
+          <Image
+            src="/images/logos/vryce-white-on-transparent-ori.png"
+            alt="Vryce Logo"
+            width={100}
+            height={27}
+            className="w-30"
+          />
         ) : (
-          <Image src="/images/logos/BONUS_LOGO.png" alt="Vryce Logo" width={100} height={26} className="w-24" />
+          <Image
+            src="/images/logos/vryce-black-on-transparent-ori.png"
+            alt="Vryce Logo"
+            width={100}
+            height={26}
+            className="w-24"
+          />
         )}
       </Link>
     </div>
